96558e7a719bad4d6207c728fa8e3f8798e0d161
Danish <sdanish.cse@gmail.com>
2017-09-03 09:37:06 +0530

    Added java project

---
+++ WithoutSynchronization/src/com/Test.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Test implements Runnable {
+
+	private static int counter = 0;
+	
+	public void run(){
+		synchronized(Test.class){
+		while(counter < 10){
+			System.out.println("["+Thread.currentThread().getName()+"] before: "+counter);
+			counter++;
+			System.out.println("["+Thread.currentThread().getName()+"] after: "+counter);
+			}		
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread[] threads = new Thread[5];
+		for(int i = 0;i < threads.length;i++){
+			threads[i] = new Thread(new Test(),"Thread "+i);
+			threads[i].start();
+		}
+		
+	}
+
+}
